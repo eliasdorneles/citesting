a541ad68d8458695028681e9e8127ab4af3652bc
Elias Dorneles <eliasdorneles@gmail.com>
2015-09-07 10:55:41 -0300

    another take at deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+set -e
+
+
 echo "Updating local copy"
 git pull origin master:master
 echo "Testing deploy"
 echo "Deploy: `date`" >> README.md
 git add README.md
 git commit -m "auto-deploy"
-git push origin master:master
+
+git remote remove deploy
+git remote add deploy https://github.com/eliasdorneles/citesting.git
+
+git push deploy master:master
