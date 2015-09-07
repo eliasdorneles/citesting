5d25fce851318662fa5118c563355c23e13b1316
Elias Dorneles <eliasdorneles@gmail.com>
2015-09-07 11:32:45 -0300

    commenting everything

--- deploy.sh
+++ deploy.sh
@@ -4,23 +4,23 @@ set -e
 
 
 echo "Updating local copy..."
-git pull origin master:master
-
-echo "Updating readme..."
-git checkout master
-echo "Deploy: `date`" >> README.md
-git add README.md
-git commit -m "auto-deploy"
-git checkout development
-
-echo "git remote -v"
-git remote -v
-# git remote add deploy https://github.com/eliasdorneles/citesting.git || echo "Deploy already exists, whatever..."
+# git pull origin master:master
+#
+# echo "Updating readme..."
+# git checkout master
+# echo "Deploy: `date`" >> README.md
+# git add README.md
+# git commit -m "auto-deploy"
+# git checkout development
+#
+# echo "git remote -v"
 # git remote -v
-
-echo "Adding key..."
-chmod 600 .travis/deploy_key.pem
-ssh-add .travis/deploy_key.pem
-
-echo "Pushing to master..."
-git push origin master:master
+# # git remote add deploy https://github.com/eliasdorneles/citesting.git || echo "Deploy already exists, whatever..."
+# # git remote -v
+#
+# echo "Adding key..."
+# chmod 600 .travis/deploy_key.pem
+# ssh-add .travis/deploy_key.pem
+#
+# echo "Pushing to master..."
+# git push origin master:master
