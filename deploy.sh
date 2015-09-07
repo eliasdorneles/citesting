2f3861f07478a3a6743521224d19860a53116159
Elias Dorneles <eliasdorneles@gmail.com>
2015-09-07 11:03:05 -0300

    trying again with origin

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,10 @@ echo "Deploy: `date`" >> README.md
 git add README.md
 git commit -m "auto-deploy"
 
+echo "git remote -v"
 git remote -v
-git remote add deploy https://github.com/eliasdorneles/citesting.git || echo "Deploy already exists, whatever..."
+# git remote add deploy https://github.com/eliasdorneles/citesting.git || echo "Deploy already exists, whatever..."
+# git remote -v
 
-git push deploy master:master
+echo "Pushing to master..."
+git push origin master:master
