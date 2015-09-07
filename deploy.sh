256d9d1c7a3e07413a847bf90ed3bd6506f7a070
Elias Dorneles <eliasdorneles@gmail.com>
2015-09-07 11:37:44 -0300

    restore old script and attempt decrypting in deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,23 +4,27 @@ set -e
 
 
 echo "Updating local copy..."
-# git pull origin master:master
-#
-# echo "Updating readme..."
-# git checkout master
-# echo "Deploy: `date`" >> README.md
-# git add README.md
-# git commit -m "auto-deploy"
-# git checkout development
-#
-# echo "git remote -v"
-# git remote -v
-# # git remote add deploy https://github.com/eliasdorneles/citesting.git || echo "Deploy already exists, whatever..."
-# # git remote -v
-#
-# echo "Adding key..."
-# chmod 600 .travis/deploy_key.pem
-# ssh-add .travis/deploy_key.pem
-#
-# echo "Pushing to master..."
-# git push origin master:master
+git pull origin master:master
+
+echo "Updating readme..."
+git checkout master
+echo "Deploy: `date`" >> README.md
+git add README.md
+git commit -m "auto-deploy"
+git checkout development
+
+echo "git remote -v"
+git remote -v
+
+echo "Restoring key..."
+mkdir -p .travis
+openssl aes-256-cbc -K $encrypted_ef748fe1608c_key -iv $encrypted_ef748fe1608c_iv \
+    -in deploy_key.pem.enc -out .travis/deploy_key.pem -d
+
+
+echo "Adding key..."
+chmod 600 .travis/deploy_key.pem
+ssh-add .travis/deploy_key.pem
+
+echo "Pushing to master..."
+git push origin master:master
